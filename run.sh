4c4c686cd7bf5b4905f3b849d2d512a7cdf42ae5
lauren <lauren@example.com>
2026-08-29 20:58:17 +0000

    remove unneeded .gitignore

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ podman run -it \
            --volume ~/.ssh/known_hosts:/root/.ssh/known_hosts:z,ro \
            --volume .:/root/workspace:Z \
            --volume ~/.cargo/credentials.toml:/root/.cargo/credentials.toml:z,ro \
+           --volume ~/.gitconfig:/root/.gitconfig:z,ro \
            --mount source=global-cargo-registry-cache,target=/root/.cargo/registry,type=volume \
            --mount source=global-cargo-git-cache,target=/root/.cargo/git,type=volume \
            --mount source=local-cargo-target-cache-"$cwd_name",target=/root/workspace/target,type=volume \
