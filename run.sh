4172ffe6e7cbce16e810630bc65b1f6fd2c8d592
lauren <lauren@example.com>
2026-08-29 16:44:59 -0400

    correct uid/gid issue; use named mounts for caching cargo downloads

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+cwd_name=$(basename $(pwd))
+
 podman run -it \
            --init \
-           --workdir /home/cargo/workspace \
-           --volume ~/.ssh/id_rsa:/root/.ssh/id_rsa:Z,ro \
-           --volume ./rusttest:/home/cargo/workspace:Z \
-           --mount source=cargo-cache,target=/home/cargo/.cargo,type=volume \
-           --mount source=target-cache,target=/home/cargo/workspace/target,type=volume \
-           --env CARGO_HOME=/home/cargo/.cargo \
-           --userns=keep-id \
-           --user root \
+           --workdir /root/workspace \
+           --volume ~/.ssh/id_rsa:/root/.ssh/id_rsa:z,ro \
+           --volume ~/.ssh/known_hosts:/root/.ssh/known_hosts:z,ro \
+           --volume .:/root/workspace:Z \
+           --volume ~/.cargo/credentials.toml:/root/.cargo/credentials.toml:z,ro \
+           --mount source=global-cargo-registry-cache,target=/root/.cargo/registry,type=volume \
+           --mount source=global-cargo-git-cache,target=/root/.cargo/git,type=volume \
+           --mount source=local-cargo-target-cache-"$cwd_name",target=/root/workspace/target,type=volume \
+           --env CARGO_HOME=/root/.cargo \
            -- fun bash
